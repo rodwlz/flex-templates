8a2d408238fe2615d150f9c906bcdce1a18a5467
rodwlz <zapatarzg@gmail.com>
2024-03-20 14:07:31 +0100

    fullstack v1.0.1

--- src/api/backend.py
+++ src/api/backend.py
@@ -11,7 +11,11 @@ route_modules = ["users", "products",]
 # Import and include each route module one by one
 for route_module_name in route_modules:
     try:
-        route_module = importlib.import_module(f"api.{route_module_name}")
+        route_module = importlib.import_module(f"src.api.{route_module_name}")
         backend.include_router(route_module.router)
     except ModuleNotFoundError:
-        print(f"api module '{route_module_name}' not found.")
+        print(f"Route module '{route_module_name}' not found.")
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(backend, host="127.0.0.1", port=8080)
